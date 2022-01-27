9c280201251f6a0244394270e3df0839ffe14991
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2022-01-27 20:43:02 +0900

    revise
    revise

---
+++ JS/contactButton.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('.contactMe').mouseover(function(){
+    $('.contactMe_tootip').fadeIn();
+  });
+  $('.contactMe').mouseout(function(){
+    $('.contactMe_tootip').fadeOut();
+  });
+});
