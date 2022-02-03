c7d6f3d9acac17ce384b85fb1216adad4ccfc7cd
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2022-02-03 20:53:57 +0900

    revise
    revise

--- JS/fadeIn.js
+++ JS/fadeIn.js
@@ -2,11 +2,10 @@ $(function () {
   $('.portfolio1').mouseover(function () {
     $(this).fadeIn(3000, function () {
       $(this).css('background-color', 'rgb(148,187,233)')
+      $(this).css('transition','all 3s')
     });
   });
   $('.portfolio1').mouseout(function () {
-    $(this).fadeOut(1000, function(){
-      $(this).css('background-color', 'transparent')
-    });
+      $(this).removeClass()
   });
 });
--- JS/scroll.js
+++ JS/scroll.js
@@ -9,7 +9,19 @@ $(window).scroll(function () {
   if (wb > 550) {
     $('.bubble').css("transform", "translateY(0px)").css("opacity", "1");
   }
-  if (wb > 750){
+  if (wb > 600) {
+    $('.company1').css("transform", "translateY(0px)").css("opacity", "1");
+  }
+  if (wb > 650) {
+    $('.bubble2').css("transform", "translateY(0px)").css("opacity", "1");
+  }
+  if (wb > 700) {
+    $('.company2').css("transform", "translateY(0px)").css("opacity", "1");
+  }
+  if (wb > 1200){
     $('.whale').css("transform", "translateY(0px)").css("opacity", "0.5");
   }
+  else if(wb < 1200){
+    $('.whale').css("transform", "translateY(0px)").css("opacity", "0");
+  };
 });
