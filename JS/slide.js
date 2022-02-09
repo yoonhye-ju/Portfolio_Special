68824f787722776113dcd9f98a5ea48d2f859008
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2022-02-09 17:57:08 +0900

    revise
    revise

--- JS/slide.js
+++ JS/slide.js
@@ -1,24 +1,24 @@
 $(function () {
-function prev() {
-  $('.slide:last').prependTo('#programBox');
-  $('#programBox').css('margin-left', -360);
-  $('#programBox').stop().animate({ marginLeft: 0 }, 800)
-}
+  function prev() {
+    $('.slide:last').prependTo('#programBox');
+    $('#programBox').css('margin-left', -360);
+    $('#programBox').animate({ marginLeft: 0 }, 800)
+  }
 
-function next() {
-  $('#programBox').stop().animate({ marginLeft: -360 }, function () {
-    $('.slide:first').appendTo('#programBox');
-    $('#programBox').css({ marginLeft: 0 });
-  });
-}
+  function next() {
+    $('#programBox').animate({ marginLeft: -360 }, function () {
+      $('.slide:first').appendTo('#programBox');
+      $('#programBox').css({ marginLeft: 0 });
+    });
+  }
 
-function slide() {
-  $('#programBox').stop().animate({ marginLeft: -360 }, function () {
-    $('.slide:first').appendTo('#programBox');
-    $('#programBox').css({ marginLeft: 0 });
-  });
-}
-setInterval(slide, 3000);
+  // function slide() {
+  //   $('#programBox').stop().animate({ marginLeft: -360 }, function () {
+  //     $('.slide:first').appendTo('#programBox');
+  //     $('#programBox').css({ marginLeft: 0 });
+  //   });
+  // }
+  // setInterval(slide, 3000);
 
   $('.prev').click(function () {
     prev();
@@ -27,6 +27,3 @@ setInterval(slide, 3000);
     next();
   });
 });
-
-
-
