2f7408d2d6539c5f929eeff066b794a77dc1ddd8
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2022-02-09 19:18:13 +0900

    revise
    revise

--- JS/slide.js
+++ JS/slide.js
@@ -1,24 +1,23 @@
 $(function () {
-  function prev() {
+  function next() {
     $('.slide:last').prependTo('#programBox');
     $('#programBox').css('margin-left', -360);
     $('#programBox').animate({ marginLeft: 0 }, 800)
   }
 
-  function next() {
+  function prev() {
     $('#programBox').animate({ marginLeft: -360 }, function () {
       $('.slide:first').appendTo('#programBox');
       $('#programBox').css({ marginLeft: 0 });
     });
   }
 
-  // function slide() {
-  //   $('#programBox').stop().animate({ marginLeft: -360 }, function () {
-  //     $('.slide:first').appendTo('#programBox');
-  //     $('#programBox').css({ marginLeft: 0 });
-  //   });
-  // }
-  // setInterval(slide, 3000);
+  function slide() {
+    $('.slide:last').prependTo('#programBox');
+    $('#programBox').css({ marginLeft: -360 });
+    $('#programBox').animate({ marginLeft: 0 });
+  }
+  setInterval(slide, 3000);
 
   $('.prev').click(function () {
     prev();
